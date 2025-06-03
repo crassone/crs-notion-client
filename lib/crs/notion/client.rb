06eb8db83c6da66db1fc1f9b60c52fc418cf9376
nagamoto <takahiro_nagamoto@crassone.jp>
2025-06-03 16:05:14 +0900

    cop対応。

--- lib/crs/notion/client.rb
+++ lib/crs/notion/client.rb
@@ -28,15 +28,15 @@ module Crs
 
       def create_database_item(database_id:, properties:)
         endpoint = "https://api.notion.com/v1/pages"
-        
+
         body = {
           parent: { database_id: database_id },
           properties: properties
         }
-        
+
         response = ::HTTParty.post(endpoint, headers: headers, body: body.to_json)
         raise "Notion API Error: #{response.code}, #{response.message}" if response.code != 200
-        
+
         JSON.parse(response.body)
       end
 
@@ -46,14 +46,14 @@ module Crs
       # @return [Hash] 更新されたアイテムの情報
       def update_database_item(item_id:, properties:)
         endpoint = "https://api.notion.com/v1/pages/#{item_id}"
-        
+
         body = {
           properties: properties
         }
-        
+
         response = ::HTTParty.patch(endpoint, headers: headers, body: body.to_json)
         raise "Notion API Error: #{response.code}, #{response.message}" if response.code != 200
-        
+
         JSON.parse(response.body)
       end
     end
